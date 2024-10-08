907242e79e25790fecf7a8f4c0c9f20044593f5a
Лёха домашний <macarena7777777@gmail.com>
2024-10-08 22:44:47 +0300

    11.7 JS integration

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const value = "Какое-то значение";
+const items = ["Элемент 1", "Элемент 2", "Элемент 3"]
+
 root.render(
   <div>
-    <h1>Какой-то текст</h1>
+    <h1>{value}</h1>
     <ul>
-      <li>Lorem ipsum dolor sit amet.</li>
-      <li>Molestiae, qui! Architecto, hic odio.</li>
-      <li>Nam maiores ut perferendis perspiciatis!</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+      <li>{items[2]}</li>
     </ul>
   </div>
 );
+
+
 
 // const h1Element = document.createElement("h1");
 // h1Element.innerText = "Hello world";
